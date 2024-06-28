af2276da4c635b84287c4dbcd95c0c3e39ab35eb
zamanwebhunters <zaman@thewebhunters.com>
2024-06-29 00:31:41 +0500

    changes

--- server.js
+++ server.js
@@ -70,8 +70,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 module.exports = app;
